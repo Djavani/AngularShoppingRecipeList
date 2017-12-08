7487fc13dbe6ef3f76f0261423d1378be3ef8f8b
Djavani Gomes <drgomes5@stefanini.com>
2017-12-08 11:07:07 -0200

    Refactor - Passing Recipe Data with Event and Property Binding (combined) and Adding ingredients to  the shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Ingredient } from './../shared/ingedient.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -18,4 +18,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
